1e9a5f631a062eb6351ec8876a059ee465c16d89
Oualidaz <oualidazdev@gmail.com>
2023-08-27 16:33:52 +0200

    Added Layout and removed JS

--- src/routes/project-eaa/AirsoftTag/+page.svelte
+++ src/routes/project-eaa/AirsoftTag/+page.svelte
@@ -1,10 +1,86 @@
+
 <div
-  class=" pl-12 pt-16 flex flex-col"
+  class="px-12 m-5 bg-yellow-300 text-neutral-700 rounded-2xl"
 >
-  <h1
+  <p class="pl-12 pt-5 ">NOTICE</p>
+  <p class="px-4 pb-5">Currently, AirsoftTag, the site and the game servers are being developed.</p>
+</div>
+
+<div
+  class=" h-[890px] items-center px-12 flex "
+>
+  <div
+    class="flex flex-col grow"
+  >
+    <h1
     class="text-5xl"
   >AirsoftTag</h1>
   <h2
     class="text-xl"
   >Insert Slogan here</h2>
+  </div>
+  
+  <h2
+    class="text-xl"
+  >Try Now!</h2>
+</div>
+
+
+
+
+<div
+  class="bg-slate-400 h-[500px] px-12 py-5 flex flex-col items-center "
+>
+  <p class="py-1 text-3xl text-center grow">Meet The First Character!</p>
+  <p class="p-4">Yuan Xiao</p>
+</div>
+
+<div class="bg-slate-500 h-24">
+  <div>
+    Next Stop:
+  </div>
+  <div
+    class="flex flex-row justify-around"
+  >
+    <p>TAIPEI</p>
+    <p class=" text-center absolute">MAP</p>
+    <p>Hon Lan Deng Qu</p>
+  </div>
+</div>
+
+
+<div
+  class="p-5"
+>
+  <h1
+    class="text-center"
+  >Highlight</h1>
+  <div
+    class="m-5 bg-slate-600 aspect-[3/2]"
+  >
+
+  </div>
+</div>
+
+<div
+  class="m-10"
+>
+  <h1
+    class="text-center"
+  >Latest News</h1>
+  <div
+    class=" my-5 bg-slate-600 aspect-[3/1]"
+  >
+  </div>
+  <div
+    class="my-5 bg-slate-600 aspect-[3/1]"
+  >
+  </div>
+  <div
+    class="my-5 bg-slate-600 aspect-[3/1]"
+  >
+  </div>
+  <button
+    class="py-2 w-full bg-orange-300 text-center"
+  >All News</button>
 </div>
